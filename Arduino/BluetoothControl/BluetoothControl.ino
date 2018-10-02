9446c5a21b42217d18f11a2c20b6d1801393f694
leistob <tobiasleis8@gmail.com>
2018-10-02 22:12:11 +0200

    Adding comment section to Arduino code, refactoring code

--- Arduino/BluetoothControl/BluetoothControl.ino
+++ Arduino/BluetoothControl/BluetoothControl.ino
@@ -1,43 +1,41 @@
 /* -------------------------------------------
-/* Bluetooth Seriell senden und empfangen
-/* www.frag-duino.de
-/* -------------------------------------------
-/* Befehle:
-/* Sendet alles, was eingegeben wird
-/* Empfaengt alles, was gesendet wurde
- ------------------------------------------- */
+ * BluetoothControl
+ * 
+ * Script for Arduino UNO and the HC-06 Bluetooth module. The well-known library SoftwareSerial
+ * is used for the communication with the Bluetooth module. No further hardware except the named
+ * module is needed.
+ * This code is primary used to receive messages from an Android device and send back a response.
+ * 
+ *
+ * Created:   01.10.2018
+ * by:        Tobias Leis
+ * Modified:  02.10.2018
+ * by:        Tobias Leis
+ * 
+ * Full tutorial and Android code:
+ * LINK
+ * ------------------------------------------- */
  
-// Konstanten
-#define PIN_RECEIVE 2
-#define PIN_SEND 3
-#define PIN_STATUS 13
-#define SPEED_BLUETOOTH 9600
-#define SPEED_SERIAL 9600
- 
-// INEX BT Board 9600
-// Linkmatic 115200
- 
-// Includes
 #include <SoftwareSerial.h>
  
-// Variablen
+#define PIN_RECEIVE 2
+#define PIN_SEND 3
+#define SPEED_BLUETOOTH 9600
+#define SPEED_SERIAL 9600
+
 SoftwareSerial blueSerial(PIN_RECEIVE, PIN_SEND);
-int zustand_status = HIGH;
- 
+
 void setup() {
   Serial.begin(SPEED_SERIAL);
-  Serial.println("Bluetooth Serial initialisiert");
-  pinMode(PIN_STATUS, OUTPUT);
- 
-  // auf seriellen Port horchen
+  Serial.println("Initialized.");
+  
   blueSerial.begin(SPEED_BLUETOOTH);
-  blueSerial.println("Bluetooth Serial Verbindung hergestellt");
+  blueSerial.println("Bluetooth connection successful.");
 }
  
-void loop() {
-  // Sendet alles, was eingegeben wird.
+void loop() {  
   if (Serial.available())
-    blueSerial.write(Serial.read());
+    blueSerial.write(Serial.read() + "\n");
  
   // Gibt alles aus, was empfangen wird:
   if (blueSerial.available()){
